ac5991ddc8095d77c27c644c9522d5fc16ec94e9
tviskaron <tviskaron@gmail.com>
2020-01-22 18:57:36 +0300

    newer version. new code for solution and comments

--- codehere/code_here.py
+++ codehere/code_here.py
@@ -2,26 +2,34 @@
 import re
 import json
 import argparse
-import os
 from copy import deepcopy
 
 
 class FileRender:
-    _SEP_BEGIN = '"""<code>"""'
-    _SEP_END = '"""</code>"""'
-    _CODE_REPLACEMENT = "raise NotImplementedError"
-    _SEP_TEMPLATE = r'^[\s\t]*{0}[\s\t]*$'
+    SEP_TEMPLATE = r'^[\s\t]*{0}[\s\t]*$'
 
-    @classmethod
-    def check_separators_consistency(cls, lines):
+    SEP_BEGIN = '"""<codehere>"""'
+    SEP_END = '"""</codehere>"""'
+
+    COMMENT_BEGIN = '"""<comment>"""'
+    COMMENT_END = '"""</comment>"""'
+
+    CODE_REPLACEMENT = "raise NotImplementedError"
+
+    def __init__(self, sep_begin=SEP_BEGIN, sep_end=SEP_END, code_replacement=CODE_REPLACEMENT):
+        self.begin_sep = sep_begin
+        self.end_sep = sep_end
+        self.code_replacement = code_replacement
+
+    def check_separators_consistency(self, lines):
         unclosed_begin_separators = 0
         for line in lines:
-            if cls.is_begin_sep(line) and cls.is_end_sep(line):
+            if self.is_begin_sep(line) and self.is_end_sep(line):
                 raise KeyError('two separators in same line')
 
-            if cls.is_begin_sep(line):
+            if self.is_begin_sep(line):
                 unclosed_begin_separators += 1
-            if cls.is_end_sep(line):
+            if self.is_end_sep(line):
                 unclosed_begin_separators -= 1
 
             # TODO show lines where error appear
@@ -31,47 +39,44 @@ class FileRender:
         if unclosed_begin_separators != 0:
             raise KeyError("latest scope did not closed")
 
-    @classmethod
-    def get_code_replacement(cls):
-        return cls._CODE_REPLACEMENT
+    def get_code_replacement(self):
+        return self.code_replacement
 
-    @classmethod
-    def get_begin_sep(cls):
-        return cls._SEP_BEGIN
+    def get_begin_sep(self):
+        return self.begin_sep
 
-    @classmethod
-    def get_end_sep(cls):
-        return cls._SEP_END
+    def get_end_sep(self):
+        return self.end_sep
 
-    @classmethod
-    def is_begin_sep(cls, line):
-        return re.search(cls._SEP_TEMPLATE.format(cls._SEP_BEGIN), line)
+    def is_begin_sep(self, line):
+        return re.search(self.SEP_TEMPLATE.format(self.begin_sep), line)
 
-    @classmethod
-    def is_end_sep(cls, line):
-        return re.search(cls._SEP_TEMPLATE.format(cls._SEP_END), line)
+    def is_end_sep(self, line):
+        return re.search(self.SEP_TEMPLATE.format(self.end_sep), line)
 
-    @classmethod
-    def is_sep(cls, line):
-        return cls.is_begin_sep(line) or cls.is_end_sep(line)
+    def is_sep(self, line):
+        return self.is_begin_sep(line) or self.is_end_sep(line)
 
-    @classmethod
-    def get_separators_indexes(cls, lines):
-        cls.check_separators_consistency(lines)
-        return [index for index, line in enumerate(lines) if cls.is_sep(line)]
+    def get_separators_indexes(self, lines):
+        self.check_separators_consistency(lines)
+        return [index for index, line in enumerate(lines) if self.is_sep(line)]
 
-    def render_text_block(self, lines, begin_rep="#" * 6 + " Your code here " + "#" * 10, end_rep="##" + "#" * 30):
+    def render_text_block(self, lines, begin_rep="#" * 6 + "# Здесь ваш код " + "#" * 10, end_rep="##" + "#" * 30):
+    # def render_text_block(self, lines, begin_rep="#" * 6 + " Your code here " + "#" * 10, end_rep="##" + "#" * 30):
         # TODO fix encodings
         lines = deepcopy(lines)
-
         rev_sep_indexes = list(reversed(self.get_separators_indexes(lines)))
         if not rev_sep_indexes:
             return lines
-
         for begin, end in zip(rev_sep_indexes[1::2], rev_sep_indexes[::2]):
-            middle = [lines[begin].replace(self.get_begin_sep(), begin_rep),
-                      lines[begin].replace(self.get_begin_sep(), self.get_code_replacement()),
-                      lines[end].replace(self.get_end_sep(), end_rep)]
+            if self.code_replacement is None:
+                middle = [lines[begin].replace(self.get_begin_sep(), begin_rep),
+                          *lines[begin+1:end],
+                          lines[end].replace(self.get_end_sep(), end_rep)]
+            else:
+                middle = [lines[begin].replace(self.get_begin_sep(), begin_rep),
+                          lines[begin].replace(self.get_begin_sep(), self.get_code_replacement()),
+                          lines[end].replace(self.get_end_sep(), end_rep)]
             lines = lines[:begin] + middle + lines[end + 1:]
         return lines
 
@@ -90,17 +95,25 @@ def main():
                                         
                                         Replaces lines with {0} and {1} separators with 
                                         raise NotImplementedError code inside. 
-                                     """.format(FileRender.get_begin_sep(), FileRender.get_end_sep()))
+                                     """.format(FileRender().get_begin_sep(), FileRender().get_end_sep()))
     parser.add_argument('file', type=str, help='path to input file')
     parser.add_argument('--outfile', type=str, help='path to output file')
     parser.add_argument('--clear', action='store_true', help='clear code cells outputs')
+    parser.add_argument('--solution', action='store_true', help="solution instead of 'NotImplemented'")
 
     args = parser.parse_args()
-
     if args.file.endswith(".py"):
         with open(args.file, "r") as in_:
             lines = in_.readlines()
-            rendered_block = FileRender().render_text_block(lines)
+            if args.solution:
+                rendered_block = FileRender(code_replacement=None).render_text_block(lines)
+            else:
+                rendered_block = FileRender().render_text_block(lines)
+            rendered_block = FileRender(sep_begin=FileRender.COMMENT_BEGIN,
+                                                sep_end=FileRender.COMMENT_END,
+                                                code_replacement="").render_text_block(rendered_block,
+                                                                                       begin_rep="",
+                                                                                       end_rep="")
             with open(get_out_file_name(args), "w") as out:
                 out.writelines(rendered_block)
 
@@ -111,12 +124,20 @@ def main():
 
             for cell in cells:
                 if cell['cell_type'] == 'code':
-                    cell['source'] = FileRender().render_text_block(cell['source'])
+                    if args.solution:
+                        cell['source'] = FileRender(code_replacement=None).render_text_block(cell['source'])
+                    else:
+                        cell['source'] = FileRender().render_text_block(cell['source'])
+                    cell['source'] = FileRender(sep_begin=FileRender.COMMENT_BEGIN,
+                                                sep_end=FileRender.COMMENT_END,
+                                                code_replacement="").render_text_block(cell['source'],
+                                                                                       begin_rep="",
+                                                                                       end_rep="")
                     if args.clear:
                         cell["outputs"] = []
 
             with open(get_out_file_name(args), "w") as out:
-                json.dump(notebook_text, out)
+                json.dump(notebook_text, out, ensure_ascii=False)
     else:
         parser.error("Unrecognized extension of " + args.file)
 
